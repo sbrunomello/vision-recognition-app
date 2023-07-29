43d8234b107baf38ac30337271224b43c74dc06c
Bruno Mello <sbrunomello@gmail.com>
2023-07-28 22:47:46 -0300

    start

---
+++ src/main/java/com/mello/visionrecognitionapp/VisionRecognitionAppApplication.java
@@ -0,0 +1,13 @@
+package com.mello.visionrecognitionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionRecognitionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionRecognitionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mello/visionrecognitionapp/VisionRecognitionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mello.visionrecognitionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionRecognitionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
